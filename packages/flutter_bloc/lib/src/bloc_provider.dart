a805587a607322e843643a7f18ae8cb4527fe698
Felix Angelov <felangelov@gmail.com>
2019-01-01 19:52:42 -0600

    Revert to 91fd7ef

--- packages/flutter_bloc/lib/src/bloc_provider.dart
+++ packages/flutter_bloc/lib/src/bloc_provider.dart
@@ -1,79 +1,87 @@
-import 'dart:collection';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
-
-import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
 
 /// A Flutter widget which provides a bloc to its children via `BlocProvider.of(context)`.
 /// It is used as a DI widget so that a single instance of a bloc can be provided
 /// to multiple widgets within a subtree.
 class BlocProvider extends StatefulWidget {
-  /// The Blocs which are to be made available throughout the subtree
+  /// The Blocs which are to be made available throughout the subtree.
+  /// If multiple blocs are provided, the blocs will be inserted in the same order they are specified.
+  /// For example if blocs: [BlocA, BlocB] BlocA will the an ancestor of BlocB.
+  /// Blocs must not be null or empty.
   final List<Bloc> blocs;
 
-  /// The Widget and its descendants which will have access to the Blocs.
+  /// The Widget and its descendants which will have access to the Bloc.
+  /// This must not be null.
   final Widget child;
 
   BlocProvider({
     Key key,
     @required this.blocs,
     @required this.child,
-  })  : assert(blocs != null && blocs.isNotEmpty && !blocs.contains(null)),
+  })  : assert(blocs != null && blocs.isNotEmpty),
         assert(child != null),
         super(key: key);
 
-  static HashMap<Type, HashMap<Type, Bloc>> _blocs =
-      HashMap<Type, HashMap<Type, Bloc>>();
+  @override
+  _BlocProviderState createState() => _BlocProviderState();
 
   /// Method that allows widgets to access the bloc as long as their `BuildContext`
   /// contains a `BlocProvider` instance.
   static B of<B extends Bloc<dynamic, dynamic>>(BuildContext context) {
-    final Type contextType = context.owner.runtimeType;
-    if (_blocs[contextType] == null) {
-      throw FlutterError(
-          'BlocProvider.of() called with a context that does not contain any BlocProviders.\n'
-          'This can happen if the context you use comes from a widget above the BlocProvider.\n'
-          'The context used was:\n'
-          '  $context');
-    }
-    if (_blocs[contextType][B] == null) {
+    final type = _typeOf<_BlocProviderInherited<B>>();
+    print('of $type');
+    final _BlocProviderInherited<B> provider =
+        context.ancestorInheritedElementForWidgetOfExactType(type)?.widget;
+
+    if (provider == null) {
       throw FlutterError(
           'BlocProvider.of() called with a context that does not contain a Bloc of type $B.\n'
-          'This can happen if the context you use comes from a widget above the BlocProvider.\n'
+          'No ancestor could be found starting from the context that was passed '
+          'to BlocProvider.of<$B>(). This can happen '
+          'if the context you use comes from a widget above the BlocProvider.\n'
           'The context used was:\n'
           '  $context');
     }
-    return _blocs[contextType][B] as B;
+    return provider?.bloc;
   }
 
-  @override
-  _BlocProviderState createState() => _BlocProviderState();
+  static Type _typeOf<B>() => B;
 }
 
 class _BlocProviderState extends State<BlocProvider> {
   @override
-  void initState() {
-    final Type contextType = context.owner.runtimeType;
-    for (int i = 0; i < widget.blocs.length; i++) {
-      final Bloc bloc = widget.blocs[i];
-      if (BlocProvider._blocs[contextType] == null) {
-        BlocProvider._blocs[contextType] = HashMap<Type, Bloc>();
-      }
-      BlocProvider._blocs[contextType][bloc.runtimeType] = bloc;
-    }
-    super.initState();
+  Widget build(BuildContext context) =>
+      _buildBlocProviders(widget.blocs, widget.child);
+
+  Widget _buildBlocProviders(List<Bloc> blocs, Widget child) {
+    return blocs.length == 1
+        ? _BlocProviderInherited(
+            bloc: blocs.first,
+            child: child,
+          )
+        : _BlocProviderInherited(
+            bloc: blocs.removeAt(0),
+            child: _buildBlocProviders(blocs, child),
+          );
+  }
+}
+
+class _BlocProviderInherited<B extends Bloc<dynamic, dynamic>>
+    extends InheritedWidget {
+  _BlocProviderInherited({
+    Key key,
+    @required Widget child,
+    @required this.bloc,
+  }) : super(key: key, child: child) {
+    print('building ${_typeOf<B>()}');
   }
 
-  @override
-  void dispose() {
-    final Type contextType = context.owner.runtimeType;
-    BlocProvider._blocs[contextType] = HashMap<Type, Bloc>();
-    super.dispose();
-  }
+  final B bloc;
+
+  static Type _typeOf<B>() => B;
 
   @override
-  Widget build(BuildContext context) {
-    return widget.child;
-  }
+  bool updateShouldNotify(_BlocProviderInherited oldWidget) => false;
 }
